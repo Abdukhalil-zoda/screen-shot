3953370e001c6374d81caa5d4b2cbf6566cb1807
Abdulaziz <abdu3332099@gmail.com>
2021-12-15 11:51:32 +0500

    Добавьте файлы проекта.

---
+++ screen shot/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace screen_shot
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            double screenLeft = SystemParameters.VirtualScreenLeft;
+            double screenTop = SystemParameters.VirtualScreenTop;
+            double screenWidth = SystemParameters.VirtualScreenWidth;
+            double screenHeight = SystemParameters.VirtualScreenHeight;
+            var ff = System.IO.Path.GetTempFileName();
+            using (Bitmap bmp = new Bitmap((int)screenWidth,
+            (int)screenHeight))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    String filename = ff; // DateTime.Now.ToString("dd-MM-yyyyThh-mm-ss.fff") + ".png";
+                    Opacity = .0;
+                    g.CopyFromScreen((int)screenLeft, (int)screenTop, 0, 0, bmp.Size);
+                    bmp.Save(filename);
+                    Opacity = 1;
+                }
+                
+            }
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            this.WindowStyle = WindowStyle.None;
+            img.Source = new BitmapImage(new Uri(ff));
+        }
+    }
+}
